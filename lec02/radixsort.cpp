24246080d22e1b7a6a233c61e9607f4007121ad6
webturing <zj@webturing.com>
2019-03-14 21:11:20 +0800

    第二讲：高效排序(归并、快排）

--- lec02/radixsort.cpp
+++ lec02/radixsort.cpp
@@ -1,19 +1,19 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-    int a[]={6,6,1,3,3,};
-    int n=sizeof(a)/sizeof(a[0]);
-    int b[10]={0};
+int main() {
+    int a[] = {6, 6, 1, 3, 3,};
+    int n = sizeof(a) / sizeof(a[0]);
+    int b[10] = {0};
 
-    for(int i=0;i<n;i++){
+    for (int i = 0; i < n; i++) {
         b[a[i]]++;
     }
-    for(int i=0;i<10;i++){
-        if(b[i]!=0){
-          //  for(int j=0;j<b[i];j++)
-                cout<<i<<" ";
+    for (int i = 0; i < 10; i++) {
+        if (b[i] != 0) {
+            //  for(int j=0;j<b[i];j++)
+            cout << i << " ";
         }
     }
 
