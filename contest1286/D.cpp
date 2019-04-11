8887ad7365fe5afed3daf4c94b9b21bbe00b604a
webturing <zj@webturing.com>
2019-04-11 23:02:59 +0800

    C++11 STL实现的二分插入排序

---
+++ contest1286/D.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//same as
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a;
+    while (n--) {
+        int t;
+        cin >> t;
+        auto p = lower_bound(a.begin(), a.end(), t);
+        a.insert(p, t);
+    }
+    for (auto e:a)cout << e << " ";
+    cout << endl;
+    return 0;
+}
